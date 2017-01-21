ad0164b1e1d6fd1760a2c7c26997e93e67ce850e
Jan Philipp Haller <philipp.haller@rwth-aachen.de>
2017-01-21 13:36:29 +0100

    Initial Setup

---
+++ TinLi/TinLi/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * TinLi.cpp
+ *
+ * Created: 21.01.2017 13:35:13
+ * Author : AnoBit
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
