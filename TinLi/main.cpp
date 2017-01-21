1a43be1aed3c32551ea0318ace55f78395eba556
Jan Philipp Haller <philipp.haller@rwth-aachen.de>
2017-01-21 13:59:21 +0100

    Added Controller & Board Description

--- TinLi/main.cpp
+++ TinLi/main.cpp
@@ -7,7 +7,27 @@
 
 #include <avr/io.h>
 
+//Pinout
+/*									_________
+	  (PCINT5/(~RESET)/ADC0/dW)	PB5	|°		|	VCC
+(PCINT3/XTAL1/CLKI/~(OC1B)/ADC3)PB3	|		|	PB2	(SCK/USCK/SCL/ADC1/T0/INT0/PCINT2)
+(PCINT4/XTAL2/CLKO/OC1B/ADC2)	PB4 |		|	PB1	(MISO/DO/AIN1/OC0B/OC1A/PCINT1)
+								GND |_______|	PB0	(MOSI/DI/SDA/AIN0/OC0A/~(OC1A)/AREF/PCINT0)
+*/
 
+//Böärd Cönnectiöns
+/*
+
+PIN		USAGE					DESCRIPTION
+
+PB0		GPIO 1					Software UART / SPI / I2C?
+PB1		PWM OUTPUT				PWM for Led Mosfet
+PB2		GPIO 2					Software UART / SPI / I2C?
+PB3		POTI INPUT				Set Maximum Current (Max Duty Cycle) / Set PWM Duty Cycle
+PB4		GPIO 3 / ANALOG IN		GPIO / Set PWM Duty Cycle
+PB5		GPIO 4 / (~RESET)
+
+*/
 int main(void)
 {
     /* Replace with your application code */
