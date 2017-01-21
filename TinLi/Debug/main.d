a1447fbf89cf158db53dc01e69a8c8df3853853c
Jan Philipp Haller <philipp.haller@rwth-aachen.de>
2017-01-21 13:40:53 +0100

    Restructured Folder Added schematics

---
+++ TinLi/Debug/main.d
@@ -0,0 +1,37 @@
+main.d main.o: .././main.cpp \
+ x:\programme\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ x:\programme\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ x:\programme\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ x:\programme\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdint.h \
+ x:\programme\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ X:\Programme\Atmel\Studio\7.0\Packs\atmel\ATtiny_DFP\1.1.102\include/avr/iotn25.h \
+ X:\Programme\Atmel\Studio\7.0\Packs\atmel\ATtiny_DFP\1.1.102\include/avr/iotnx5.h \
+ x:\programme\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ x:\programme\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ x:\programme\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ x:\programme\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ x:\programme\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h
+
+x:\programme\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+x:\programme\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+x:\programme\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+x:\programme\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdint.h:
+
+x:\programme\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+X:\Programme\Atmel\Studio\7.0\Packs\atmel\ATtiny_DFP\1.1.102\include/avr/iotn25.h:
+
+X:\Programme\Atmel\Studio\7.0\Packs\atmel\ATtiny_DFP\1.1.102\include/avr/iotnx5.h:
+
+x:\programme\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+x:\programme\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+x:\programme\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+x:\programme\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+x:\programme\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
